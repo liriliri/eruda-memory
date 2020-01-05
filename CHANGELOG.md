da165b7dea27c7159adb2b4a52c77d4fb13a1c7a
surunzi <surunzi@foxmail.com>
2020-01-05 23:21:25 +0800

    release: v2.0.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+# v2.0.0 (3 Jan 2020)
+
+* feat: theme support
+
 # v1.1.1 (1 Sep 2019)
 
 * fix: remove settings when destroyed
--- eruda-memory.js
+++ eruda-memory.js
@@ -20,15 +20,17 @@
       GRAPH_Y = 40,
       GRAPH_WIDTH = w - 24,
       GRAPH_HEIGHT = 142,
-      BACK_COLOR = '#fff',
-      FORE_COLOR = '#009688',
       STEP = 2,
       NAME = 'MB'
 
-    var util = eruda.util,
-      Settings = eruda.Settings,
-      round = Math.round,
-      Tool = eruda.Tool
+    var util = eruda.util
+    var Settings = eruda.Settings
+    var round = Math.round
+    var Tool = eruda.Tool
+
+    var curTheme = util.evalCss.getCurTheme()
+    var BACK_COLOR = curTheme.background
+    var FORE_COLOR = curTheme.accent
 
     var Memory = Tool.extend({
       name: 'memory',
@@ -38,8 +40,8 @@
         this._style = util.evalCss(
           [
             '.eruda-dev-tools .eruda-tools .eruda-memory {padding: 10px !important;}',
-            'canvas {width: 100%; border-radius: 4px; box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.05), 0 1px 4px 0 rgba(0, 0, 0, 0.08), 0 3px 1px -2px rgba(0, 0, 0, 0.2);}',
-            '.eruda-not-supported {background: #f44336; color: #fff; padding: 10px; border-radius: 4px; text-align: center; box-shadow: 0 2px 2px 0 rgba(0, 0, 0, .05), 0 1px 4px 0 rgba(0, 0, 0, .08), 0 3px 1px -2px rgba(0, 0, 0, .2)}'
+            'canvas {width: 100%; border: 1px solid var(--border);}',
+            '.eruda-not-supported {background: var(--console-error-background); color: var(--console-error-foreground); padding: 10px; border: 1px solid var(--console-error-border); text-align: center;}'
           ].join('.eruda-dev-tools .eruda-tools .eruda-memory ')
         )
         this._container = container
